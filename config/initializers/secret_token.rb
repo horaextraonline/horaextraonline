16bb9507786d4a643b9069aac6d608888f411b47
Renan Sales <rananfu@Renan-Saless-MacBook-Pro.local>
2010-08-10 18:27:02 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Horaextraonline::Application.config.secret_token = 'd414e21be69f3ef3158f1cfed5973cc52287bf1d0ff84d51d87ad6f5657eb5dc0d4ce8499d1703eb33bada29622972882839caa844ae169c857ebbb100ca1e83'
