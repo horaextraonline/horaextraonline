b8101d236fcefc4f51c9db8c125a16ef141ba6c8
marano <thiagomarano@gmail.com>
2010-08-20 23:28:48 -0300

    Spec for submiting event creation form. Missing implementation.

--- spec/acceptance/events_page_spec.rb
+++ spec/acceptance/events_page_spec.rb
@@ -7,6 +7,15 @@ describe 'events page' do
     it 'should see new event form' do
       page.should have_content 'New Event'
     end
+    context 'submiting a new event' do
+      before do
+        fill_in :name, :with => 'My New Event'
+        click_button 'Create'
+      end
+      it 'should show success message' do
+        page.should have_content 'Event successfully created!'
+      end
+    end
   end
 end
 
